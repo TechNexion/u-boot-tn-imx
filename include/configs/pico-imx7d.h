254079c48e47aa6a257718f204df0f333a4fded2
Wig C <wig.cheng@technexion.com>
2015-11-20 18:06:19 +0800

    pico-imx7d: change to the correct fdt file.

--- include/configs/pico-imx7d.h
+++ include/configs/pico-imx7d.h
@@ -194,7 +194,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx7d-sdb.dtb\0" \
+	"fdt_file=imx7d-pico.dtb\0" \
 	"fdt_addr=0x83000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
