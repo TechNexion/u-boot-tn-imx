fa1f8fd2c7f52fc2f2abfffbed7b21740ea79f95
Richard Hu <richard.hu@technexion.com>
2015-12-17 11:52:01 +0800

    picosom-imx6ul: fix DDR size from 512MB to 256MB

--- include/configs/picosom-imx6ul.h
+++ include/configs/picosom-imx6ul.h
@@ -120,8 +120,8 @@
 
 
 #define CONFIG_DEFAULT_FDT_FILE "imx6ul-picosom.dtb"
-#define PHYS_SDRAM_SIZE			SZ_512M
-#define CONFIG_BOOTARGS_CMA_SIZE   ""
+#define PHYS_SDRAM_SIZE                        SZ_256M
+#define CONFIG_BOOTARGS_CMA_SIZE   "cma=96M "
 
 /* PMIC */
 #define CONFIG_POWER
