6b2bf4ecd2349a12c4939006f65617f27563fd2c
John Weber <john.weber@technexion.com>
2019-05-28 19:09:11 -0700

    axon-imx6: Check CONFIG_ENV_IS_IN_MMC, not define
    CONFIG_ENV_IS_IN_MMC should be defined in the defconfig for the board,
    not in the board's config include file. Check it here and set
    CONFIG_ENV_OFFSET and CONFIG_SYS_MMC_ENV_DEV accordingly.

--- include/configs/axon-imx6.h
+++ include/configs/axon-imx6.h
@@ -283,9 +283,10 @@
 
 #define CONFIG_ENV_SIZE			(8 * 1024)
 
-#define CONFIG_ENV_IS_IN_MMC
+#ifdef CONFIG_ENV_IS_IN_MMC
 #define CONFIG_ENV_OFFSET		(6 * 64 * 1024)
 #define CONFIG_SYS_MMC_ENV_DEV		0
+#endif
 
 #define CONFIG_PREBOOT \
 	"mmc dev ${mmcdev}; if mmc rescan; then " \
