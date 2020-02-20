75f3607de060d1d711e77bfc410c02acfa061e39
Po Cheng <po.cheng@technexion.com>
2020-02-20 13:43:48 +0800

    xore-imx8mm: Fix fitboot argument to use fdt_file with overlay instead
    - modify tnrescue.its to accept fdt_file name as configuration
    - add 8 and 10 inch panel to rescue loader (tnrescue.its)
    
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- include/configs/xore-imx8mm.h
+++ include/configs/xore-imx8mm.h
@@ -210,16 +210,19 @@
 		"else " \
 			"booti; " \
 		"fi;\0" \
-	"fitboard=xore_mipi\0" \
+	"fitov=\"\"\0" \
 	"fit_addr=0x45800000\0" \
 	"fit_high=0xffffffff\0" \
+	"fit_overlay=for ov in ${dtoverlay}; do " \
+			"echo Overlaying ${ov}...; setenv fitov \"${fitov}#${ov}\"; " \
+		"done; echo fit conf: ${fdt_file}${fitov};\0" \
 	"fitargs=setenv bootargs ${jh_clk} console=${console} root=/dev/ram0 rootwait rw " \
 		"modules-load=g_acm_ms g_acm_ms.stall=0 g_acm_ms.removable=1 g_acm_ms.file=/dev/mmcblk2 " \
 		"g_acm_ms.iSerialNumber=${ethaddr} g_acm_ms.iManufacturer=TechNexion\0" \
 	"loadfit=fatload mmc ${mmcdev}:${mmcpart} ${fit_addr} tnrescue.itb\0" \
 	"fitboot=echo Booting from FIT image ...; " \
-		"run fitargs; " \
-		"bootm ${fit_addr}#config@${soc_type}-${fitboard}\0"
+		"run fit_overlay; run fitargs; " \
+		"bootm ${fit_addr}#${fdt_file}${fitov}\0"
 
 #define CONFIG_BOOTCOMMAND \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
