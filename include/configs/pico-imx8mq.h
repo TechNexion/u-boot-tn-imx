bf78b979f1dd26e208b40514cd88e245d06e6e78
Wig Cheng <wig.cheng@technexion.com>
2020-11-26 11:21:11 +0800

    pico-imx8m: add support to load device tree blob overlay(DTBO)

--- include/configs/pico-imx8mq.h
+++ include/configs/pico-imx8mq.h
@@ -89,6 +89,7 @@
 	"console=ttymxc0,115200\0" \
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
+	"fdt_buffer=8192\0"     \
 	"boot_fdt=try\0" \
 	"fdt_file=imx8mq-pico-pi.dtb\0" \
 	"initrd_addr=0x43800000\0"		\
@@ -101,12 +102,21 @@
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
+	"baseboard=pi\0" \
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
 	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+        "loadoverlay=" \
+               "fdt addr ${fdt_addr} && fdt resize ${fdt_buffer}; " \
+               "setexpr fdtovaddr ${fdt_addr} + 0xF0000; " \
+               "for ov in ${dtoverlay}; do " \
+                       "echo Overlaying ${ov}...; " \
+                       "fatload mmc ${mmcdev}:${mmcpart} ${fdtovaddr} imx8mq-pico-${baseboard}-${ov}.dtbo && fdt apply ${fdtovaddr}; " \
+               "done\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run mmcargs; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if run loadfdt; then " \
+				"run loadoverlay; " \
 				"booti ${loadaddr} - ${fdt_addr}; " \
 			"else " \
 				"echo WARN: Cannot load the DT; " \
