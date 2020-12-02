54d7b18207cea7a20c59c898493d127b259c6317
Ray Chang <ray.chang@technexion.com>
2020-12-02 16:01:32 +0800

    pico-imx8mm: enable 5 inch ili9881c for PICO-IMX8MM

--- include/configs/pico-imx8mm.h
+++ include/configs/pico-imx8mm.h
@@ -85,6 +85,7 @@
 	"script=boot.scr\0" \
 	"image=Image\0" \
 	"splashimage=0x50000000\0" \
+	"splashpos=m,m\0" \
 	"console=ttymxc1,115200\0" \
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
