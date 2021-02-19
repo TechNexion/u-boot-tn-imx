f7a6c04cf9b6bcf18a24ced8b188ecab75f110cb
Richard Hu <richard.hu@technexion.com>
2021-02-19 06:00:11 +0000

    pico-imx8mm: detect MIPI 5 inch panel(ILI9881C) and add dtoverlay to enable it.

--- board/technexion/pico-imx8mm/pico-imx8mm.c
+++ board/technexion/pico-imx8mm/pico-imx8mm.c
@@ -288,9 +288,8 @@ void board_late_mmc_env_init(void)
 #define PCA9555_26_I2C_ADDR 0x26
 #define EXPANSION_IC_I2C_BUS 2
 
-int board_late_init(void)
+int detect_baseboard(void)
 {
-
 	struct udevice *bus = NULL;
 	struct udevice *i2c_dev = NULL;
 	int ret;
@@ -319,6 +318,52 @@ int board_late_init(void)
 		strcat(str_fdtfile, ".dtb");
 		env_set("fdt_file", str_fdtfile);
 	}
+	return 0;
+
+}
+
+int add_dtoverlay(char *ov_name)
+{
+	char *dtoverlay, arr_dtov[64];
+
+	dtoverlay = env_get("dtoverlay");
+	if (dtoverlay) {
+		strcpy(arr_dtov, dtoverlay);
+		if (!strstr(arr_dtov, ov_name)) {
+			strcat(arr_dtov, " ");
+			strcat(arr_dtov, ov_name);
+			env_set("dtoverlay", arr_dtov);
+		}
+	} else
+		env_set("dtoverlay", ov_name);
+
+	return 0;
+}
+
+int detect_display_panel(void)
+{
+	struct udevice *bus = NULL;
+	struct udevice *i2c_dev = NULL;
+	int ret;
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C, FT5336_TOUCH_I2C_BUS, &bus);
+	if (ret) {
+		printf("%s: Can't find bus\n", __func__);
+		return -EINVAL;
+	}
+	/* detect MIPI 5 inch panel(ILI9881C) */
+	ret = dm_i2c_probe(bus, FT5336_TOUCH_I2C_ADDR, 0, &i2c_dev);
+	if (! ret)
+		add_dtoverlay("ili9881c");
+
+	return 0;
+}
+
+int board_late_init(void)
+{
+
+	detect_baseboard();
+	detect_display_panel();
 
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
