45da541f3e7a007157602949f14cbf88d42aa8f8
Richard Hu <richard.hu@technexion.com>
2021-02-19 03:31:07 +0000

    pico-imx8mm: enable loading fdt overlay

--- include/configs/pico-imx8mm.h
+++ include/configs/pico-imx8mm.h
@@ -90,6 +90,7 @@
 	"console=ttymxc1,115200\0" \
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
+	"fdt_buffer=8192\0"	\
 	"boot_fit=no\0" \
 	"fdt_file=undefined\0" \
 	"initrd_addr=0x43800000\0"		\
@@ -102,14 +103,26 @@
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
+	"baseboard=undefined\0" \
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadfdt=" \
+		"echo Loading fdt_file ${fdt_file}...; " \
+		"fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadoverlay=" \
+		"fdt addr ${fdt_addr} && fdt resize ${fdt_buffer}; " \
+		"setexpr fdtovaddr ${fdt_addr} + 0xF0000; " \
+		"for ov in ${dtoverlay}; do " \
+			"echo Overlaying ${ov}...; " \
+			"echo Loading imx8mm-pico-${baseboard}-${ov}.dtbo...; " \
+			"fatload mmc ${mmcdev}:${mmcpart} ${fdtovaddr} imx8mm-pico-${baseboard}-${ov}.dtbo && fdt apply ${fdtovaddr}; " \
+		"done\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run mmcargs; " \
 		"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
 			"bootm ${loadaddr}; " \
 		"else " \
 			"if run loadfdt; then " \
+				"run loadoverlay; " \
 				"booti ${loadaddr} - ${fdt_addr}; " \
 			"else " \
 				"echo WARN: Cannot load the DT; " \
