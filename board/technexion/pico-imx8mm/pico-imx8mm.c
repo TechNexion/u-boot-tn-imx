86936c023c05f919124a45704feff62a7edbfa8b
Richard Hu <richard.hu@technexion.com>
2021-10-21 10:45:17 +0800

    pico-imx8mm: configure DRAM size in a new way
    Fit the change of DRAM bank size configuration in 'arch/arm/mach-imx/imx8m/soc.c'.

--- board/technexion/pico-imx8mm/pico-imx8mm.c
+++ board/technexion/pico-imx8mm/pico-imx8mm.c
@@ -58,11 +58,6 @@ int board_early_init_f(void)
 	return 0;
 }
 
-static int ddr_size;
-extern struct mm_region *mem_map;
-#define DRAM1_INDEX 5 /* Correspond to the index of DRAM1 of imx8m_mem_map
-						 in arch/arm/mach-imx/imx8m/soc.c */
-
 int board_phys_sdram_size(phys_size_t *size)
 {
 	if (!size)
@@ -73,26 +68,25 @@ int board_phys_sdram_size(phys_size_t *size)
 	information of DDR size into start address of TCM.
 	It'd be better to detect DDR size from DDR controller.
 	**************************************************/
-	ddr_size = readl(MCU_BOOTROM_BASE_ADDR);
+	u32 ddr_size = readl(MCU_BOOTROM_BASE_ADDR);
 
-	if (ddr_size == 0x4) {
+	switch (ddr_size) {
+	case 0x4: /* DRAM size: 4GB */
 		*size = SZ_4G;
-		mem_map[DRAM1_INDEX].size=SZ_4G;
-	}
-	else if (ddr_size == 0x3) {
+		break;
+	case 0x3: /* DRAM size: 3GB */
 		*size = SZ_3G;
-		mem_map[DRAM1_INDEX].size=SZ_3G;
-	}
-	else if (ddr_size == 0x2) {
+		break;
+	case 0x2: /* DRAM size: 2GB */
 		*size = SZ_2G;
-		mem_map[DRAM1_INDEX].size=SZ_2G;
-	}
-	else if (ddr_size == 0x1) {
+		break;
+	case 0x1: /* DRAM size: 1GB */
 		*size = SZ_1G;
-		mem_map[DRAM1_INDEX].size=SZ_1G;
-	}
-	else
+		break;
+	default:
 		puts("Unknown DDR type!!!\n");
+	}
+
 	return 0;
 }
 
@@ -444,9 +438,9 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 	cell = fdt_getprop(blob, offs, "size", NULL);
 	cma_size = fdt32_to_cpu(cell[1]);
 	cmasize = env_get("cma_size");
-	if(cmasize || ((uint32_t)(mem_map[DRAM1_INDEX].size >> 1) < cma_size)) {
+	if(cmasize || ((u64)(gd->ram_size >> 1) < cma_size)) {
 		cma_size = env_get_ulong("cma_size", 10, 320 * 1024 * 1024);
-		cma_size = min((uint32_t)(mem_map[DRAM1_INDEX].size >> 1), cma_size);
+		cma_size = min((u64)(gd->ram_size >> 1), (u64)cma_size);
 		fdt_setprop_u64(blob, offs, "size", (uint64_t)cma_size);
 	}
 
