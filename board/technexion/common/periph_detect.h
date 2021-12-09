02297126eae36de139fe2ac6cf27b8a31b7212c1
Richard Hu <richard.hu@technexion.com>
2021-12-09 16:55:45 +0800

    ARM: technexion-common: add detection mechanism for peripherals(display, camera...)

---
+++ board/technexion/common/periph_detect.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2021 TechNexion Ltd.
+ *
+ * Author: Richard Hu <richard.hu@technexion.com>
+ *
+ */
+
+#ifndef __PERIPH_DETECT_H__
+#define __PERIPH_DETECT_H__
+
+struct tn_display {
+	int	bus;
+	int	addr;
+    int id_reg;
+    int id;
+    const char *ov_name;
+    int	(*detect)(struct tn_display const *dev);
+};
+
+struct camera_cfg {
+       u8 camera_index;
+       u8 i2c_bus_index;
+       u8 eeprom_i2c_addr;
+};
+
+extern struct tn_display const displays[];
+extern size_t tn_display_count;
+
+int detect_i2c(struct tn_display const *dev);
+#endif
