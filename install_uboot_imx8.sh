1796952404fa59fa27beec535dd4bb605a968bc3
Wig Cheng <wig.cheng@technexion.com>
2021-12-21 16:57:02 +0800

    install_uboot_imx8.sh: add instruction for SC-IMX8MP

--- install_uboot_imx8.sh
+++ install_uboot_imx8.sh
@@ -238,6 +238,9 @@ usage()
     * EDM-G-IMX8MP with WB:
     ./install_uboot_imx8.sh -b imx8mp-edm-g.dtb -d /dev/sdX
 
+    * SC-IMX8MP:
+    ./install_uboot_imx8.sh -b imx8mp-sc.dtb -d /dev/sdX
+
     * TEK-IMX8MP:
     ./install_uboot_imx8.sh -b imx8mp-tek.dtb -d /dev/sdX
 
