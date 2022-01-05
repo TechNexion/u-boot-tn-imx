643f103966f09002ed7c2f50ee9a727b74eb2a56
Wig Cheng <wig.cheng@technexion.com>
2022-01-05 17:56:45 +0800

    pico-imx6ul: add support for uboot v2021
    Migrated from uboot v2020.
    Both support pico-imx6ul and pico-imx6ull.

--- board/technexion/pico-imx6ul/pico-imx6ul.c
+++ board/technexion/pico-imx6ul/pico-imx6ul.c
@@ -5,26 +5,29 @@
  * Author: Richard Hu <richard.hu@technexion.com>
  */
 
-#include <init.h>
-#include <net.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/crm_regs.h>
 #include <asm/arch/mx6-pins.h>
 #include <asm/arch/sys_proto.h>
-#include <asm/global_data.h>
 #include <asm/gpio.h>
 #include <asm/mach-imx/iomux-v3.h>
 #include <asm/io.h>
 #include <common.h>
 #include <miiphy.h>
-#include <linux/delay.h>
+#include <netdev.h>
 #include <linux/sizes.h>
 #include <usb.h>
 #include <power/pmic.h>
 #include <power/pfuze3000_pmic.h>
 #include "../../freescale/common/pfuze.h"
+
+#ifdef CONFIG_DM_VIDEO
+#include <linux/fb.h>
+#include <mxsfb.h>
+#include <splash.h>
+#endif
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -39,8 +42,72 @@ DECLARE_GLOBAL_DATA_PTR;
 #define MDIO_PAD_CTRL  (PAD_CTL_PUS_100K_UP | PAD_CTL_PUE |     \
 	PAD_CTL_DSE_48ohm   | PAD_CTL_SRE_FAST | PAD_CTL_ODE)
 
+#define ENET_PAD_CTRL  (PAD_CTL_PUS_100K_UP | PAD_CTL_PUE |     \
+	PAD_CTL_SPEED_HIGH   |                                   \
+	PAD_CTL_DSE_48ohm   | PAD_CTL_SRE_FAST)
+
+#define ENET_CLK_PAD_CTRL  (PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST)
+
 #define LCD_PAD_CTRL    (PAD_CTL_HYS | PAD_CTL_PUS_100K_UP | PAD_CTL_PUE | \
 	PAD_CTL_PKE | PAD_CTL_SPEED_MED | PAD_CTL_DSE_40ohm)
+
+#define RMII_PHY_RESET IMX_GPIO_NR(1, 28)
+#define DDR_TYPE_DET IMX_GPIO_NR(5, 1)
+
+
+static iomux_v3_cfg_t const fec_pads[] = {
+	MX6_PAD_ENET1_TX_EN__ENET2_MDC		| MUX_PAD_CTRL(MDIO_PAD_CTRL),
+	MX6_PAD_ENET1_TX_DATA1__ENET2_MDIO	| MUX_PAD_CTRL(MDIO_PAD_CTRL),
+	MX6_PAD_ENET2_TX_DATA0__ENET2_TDATA00	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_TX_DATA1__ENET2_TDATA01	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_TX_CLK__ENET2_REF_CLK2	| MUX_PAD_CTRL(ENET_CLK_PAD_CTRL),
+	MX6_PAD_ENET2_TX_EN__ENET2_TX_EN	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_RX_DATA0__ENET2_RDATA00	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_RX_DATA1__ENET2_RDATA01	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_RX_EN__ENET2_RX_EN	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET2_RX_ER__ENET2_RX_ER	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_UART4_TX_DATA__GPIO1_IO28	| MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const ddr_type_detection_pads[] = {
+	/* ddr type detection */
+	MX6_PAD_SNVS_TAMPER1__GPIO5_IO01 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static void setup_iomux_ddr_type_detection(void)
+{
+	imx_iomux_v3_setup_multiple_pads(ddr_type_detection_pads, ARRAY_SIZE(ddr_type_detection_pads));
+}
+
+static void setup_iomux_fec(void)
+{
+	imx_iomux_v3_setup_multiple_pads(fec_pads, ARRAY_SIZE(fec_pads));
+}
+
+int board_eth_init(struct bd_info *bis)
+{
+	setup_iomux_fec();
+
+	gpio_request(RMII_PHY_RESET, "enet_phy_reset");
+	gpio_direction_output(RMII_PHY_RESET, 0);
+	/*
+	 * According to KSZ8081MNX-RNB manual:
+	 * For warm reset, the reset (RST#) pin should be asserted low for a
+	 * minimum of 500μs.  The strap-in pin values are read and updated
+	 * at the de-assertion of reset.
+	 */
+	udelay(500);
+
+	gpio_direction_output(RMII_PHY_RESET, 1);
+	/*
+	 * According to KSZ8081MNX-RNB manual:
+	 * After the de-assertion of reset, wait a minimum of 100μs before
+	 * starting programming on the MIIM (MDC/MDIO) interface.
+	 */
+	udelay(100);
+
+	return fecmxc_initialize(bis);
+}
 
 static int setup_fec(void)
 {
@@ -59,36 +126,9 @@ static int setup_fec(void)
 	return 0;
 }
 
-#ifdef CONFIG_VIDEO_MXS
+#ifdef CONFIG_DM_VIDEO
+
 static iomux_v3_cfg_t const lcd_pads[] = {
-	MX6_PAD_LCD_CLK__LCDIF_CLK | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_ENABLE__LCDIF_ENABLE | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_HSYNC__LCDIF_HSYNC | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_VSYNC__LCDIF_VSYNC | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA00__LCDIF_DATA00 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA01__LCDIF_DATA01 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA02__LCDIF_DATA02 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA03__LCDIF_DATA03 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA04__LCDIF_DATA04 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA05__LCDIF_DATA05 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA06__LCDIF_DATA06 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA07__LCDIF_DATA07 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA08__LCDIF_DATA08 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA09__LCDIF_DATA09 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA10__LCDIF_DATA10 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA11__LCDIF_DATA11 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA12__LCDIF_DATA12 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA13__LCDIF_DATA13 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA14__LCDIF_DATA14 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA15__LCDIF_DATA15 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA16__LCDIF_DATA16 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA17__LCDIF_DATA17 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA18__LCDIF_DATA18 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA19__LCDIF_DATA19 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA20__LCDIF_DATA20 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA21__LCDIF_DATA21 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA22__LCDIF_DATA22 | MUX_PAD_CTRL(LCD_PAD_CTRL),
-	MX6_PAD_LCD_DATA23__LCDIF_DATA23 | MUX_PAD_CTRL(LCD_PAD_CTRL),
 	/* LCD_BLT_CTRL: GPIO for Brightness adjustment  */
 	MX6_PAD_NAND_ALE__GPIO4_IO10 | MUX_PAD_CTRL(NO_PAD_CTRL),
 	/* LCD_VDD_EN: LCD enabled */
@@ -135,6 +175,53 @@ static iomux_v3_cfg_t const uart6_pads[] = {
 static iomux_v3_cfg_t const usb_otg_pad[] = {
 	MX6_PAD_GPIO1_IO00__ANATOP_OTG1_ID | MUX_PAD_CTRL(OTG_ID_PAD_CTRL),
 };
+
+#ifdef CONFIG_SPLASH_SCREEN
+static struct splash_location imx_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x100000,
+	},
+	{
+		.name = "mmc_fs",
+		.storage = SPLASH_STORAGE_MMC,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "usb_fs",
+		.storage = SPLASH_STORAGE_USB,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "sata_fs",
+		.storage = SPLASH_STORAGE_SATA,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+};
+
+/*This function is defined in common/splash.c.
+  Declare here to remove warning. */
+int splash_video_logo_load(void);
+
+int splash_screen_prepare(void)
+{
+	imx_splash_locations[1].devpart[0] = mmc_get_env_dev() + '0';
+	int ret;
+	ret = splash_source_load(imx_splash_locations, ARRAY_SIZE(imx_splash_locations));
+	if (!ret)
+		return 0;
+	else {
+		printf("\nNo splash.bmp in boot partition!!\n");
+		printf("Using default logo!!\n\n");
+		return splash_video_logo_load();
+	}
+}
+#endif /* CONFIG_SPLASH_SCREEN */
 
 static void setup_iomux_uart(void)
 {
@@ -216,15 +303,110 @@ int board_init(void)
 
 	setup_fec();
 	setup_usb();
-#ifdef CONFIG_VIDEO_MXS
+
+#ifdef CONFIG_DM_VIDEO
 	setup_lcd();
 #endif
+
+
 	return 0;
 }
 
+int check_mmc_autodetect(void)
+{
+        char *autodetect_str = env_get("mmcautodetect");
+
+        if ((autodetect_str != NULL) && 
+                (strcmp(autodetect_str, "yes") == 0)) {
+                return 1;
+        }
+
+        return 0;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+        return dev_no;
+}
+
+void board_late_mmc_init(void)
+{
+        char cmd[32];
+        char mmcblk[32];
+        u32 dev_no = mmc_get_env_dev();
+
+        if (!check_mmc_autodetect())
+                return;
+
+        env_set_ulong("mmcdev", dev_no);
+
+        /* Set mmcblk env */
+        sprintf(mmcblk, "/dev/mmcblk%dp2 rootwait rw",
+                mmc_map_to_kernel_blk(dev_no));
+        env_set("mmcroot", mmcblk);
+
+        sprintf(cmd, "mmc dev %d", dev_no);
+        run_command(cmd, 0);
+}
+
+int board_late_init(void)
+{
+#ifdef CONFIG_ENV_IS_IN_MMC
+        board_late_mmc_init();
+#endif /* CONFIG_ENV_IS_IN_MMC */
+
+        set_wdog_reset((struct wdog_regs *)WDOG1_BASE_ADDR);
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+        if(is_mx6ul())
+                env_set("som", "imx6ul");
+        else if(is_mx6ull())
+                env_set("som", "imx6ull");
+        else
+                env_set("som", "imx6ull");
+
+        setup_iomux_ddr_type_detection();
+        gpio_request(DDR_TYPE_DET, "ddr_det");
+        gpio_direction_input(DDR_TYPE_DET);
+
+        if (!gpio_get_value(DDR_TYPE_DET))
+                env_set("memdet", "512MB");
+        else
+                env_set("memdet", "256MB");
+#endif
+
+        return 0;
+}
+
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	const int *cell;
+	int offs;
+	uint32_t cma_size;
+	unsigned int dram_size;
+
+	dram_size = imx_ddr_size() / 1024 / 1024;
+	offs = fdt_path_offset(blob, "/reserved-memory/linux,cma");
+	cell = fdt_getprop(blob, offs, "size", NULL);
+	cma_size = fdt32_to_cpu(cell[0]);
+	if (dram_size == 256) {
+		/* CMA is aligned by 32MB on i.mx8mq,
+		   so CMA size can only be multiple of 32MB */
+		cma_size = env_get_ulong("cma_size", 10, (2 * 32) * 1024 * 1024);
+		fdt_setprop_u32(blob, offs, "size", (uint64_t)cma_size);
+	}
+
+	return 0;
+}
+#endif
+
 int checkboard(void)
 {
-	puts("Board: PICO-IMX6UL-EMMC\n");
+	char board_output[32] = {0};
+	sprintf(board_output, "Board: pico-%s\n", get_som_type());
+
+	puts(board_output);
 
 	return 0;
 }
--- board/technexion/pico-imx6ul/spl.c
+++ board/technexion/pico-imx6ul/spl.c
@@ -1,103 +1,55 @@
-// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2018 Technexion Ltd.
+ *
+ * Author: Richard Hu <richard.hu@technexion.com>
+ *         Po Cheng <po.cheng@technexion.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
 
-#include <common.h>
-#include <cpu_func.h>
-#include <hang.h>
 #include <init.h>
+#include <asm/io.h>
+#include <asm/gpio.h>
 #include <asm/arch/clock.h>
-#include <asm/arch/iomux.h>
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/crm_regs.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/mx6-ddr.h>
 #include <asm/arch/mx6ul_pins.h>
 #include <asm/arch/mx6-pins.h>
-#include <asm/arch/sys_proto.h>
-#include <asm/gpio.h>
+#include <asm/mach-imx/video.h>
 #include <asm/mach-imx/iomux-v3.h>
-#include <asm/mach-imx/boot_mode.h>
-#include <fsl_esdhc_imx.h>
-#include <linux/libfdt.h>
+#include <mxc_epdc_fb.h>
+#include <mmc.h>
 #include <spl.h>
+#include <fsl_esdhc.h>
+#include <linux/libfdt.h>
 
-#if defined(CONFIG_SPL_BUILD)
+DECLARE_GLOBAL_DATA_PTR;
 
-#ifdef CONFIG_SPL_OS_BOOT
-int spl_start_uboot(void)
-{
-	/* Break into full U-Boot on 'c' */
-	if (serial_tstc() && serial_getc() == 'c')
-		return 1;
+#define DDR_TYPE_DET			IMX_GPIO_NR(5, 1)
 
-	return 0;
-}
+#ifdef CONFIG_SPL_BUILD
+
+#ifdef CONFIG_MX6
+#define IMX6UL_DRIVE_STRENGTH		0x30
 #endif
 
-#include <asm/arch/mx6-ddr.h>
-
-static struct mx6ul_iomux_grp_regs mx6_grp_ioregs = {
-	.grp_addds = 0x00000030,
-	.grp_ddrmode_ctl = 0x00020000,
-	.grp_b0ds = 0x00000030,
-	.grp_ctlds = 0x00000030,
-	.grp_b1ds = 0x00000030,
-	.grp_ddrpke = 0x00000000,
-	.grp_ddrmode = 0x00020000,
-	.grp_ddr_type = 0x00080000,
+static iomux_v3_cfg_t const ddr_type_detection_pads[] = {
+	MX6_PAD_SNVS_TAMPER1__GPIO5_IO01 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
-static struct mx6ul_iomux_ddr_regs mx6_ddr_ioregs = {
-	.dram_dqm0 = 0x00000030,
-	.dram_dqm1 = 0x00000030,
-	.dram_ras = 0x00000030,
-	.dram_cas = 0x00000030,
-	.dram_odt0 = 0x00000030,
-	.dram_odt1 = 0x00000030,
-	.dram_sdba2 = 0x00000000,
-	.dram_sdclk_0 = 0x00000030,
-	.dram_sdqs0 = 0x00000030,
-	.dram_sdqs1 = 0x00000030,
-	.dram_reset = 0x00000030,
-};
+static void setup_iomux_ddr_type_detection(void)
+{
+	SETUP_IOMUX_PADS(ddr_type_detection_pads);
+}
 
-static struct mx6_mmdc_calibration mx6_mmcd_calib = {
-	.p0_mpwldectrl0 = 0x00000000,
-	.p0_mpdgctrl0 = 0x01380134,
-	.p0_mprddlctl = 0x40404244,
-	.p0_mpwrdlctl = 0x40405050,
-};
-
-static struct mx6_ddr_sysinfo ddr_sysinfo = {
-	.dsize		= 0,
-	.cs1_mirror	= 0,
-	.cs_density	= 32,
-	.ncs		= 1,
-	.bi_on		= 1,
-	.rtt_nom	= 1,
-	.rtt_wr		= 0,
-	.ralat		= 5,
-	.walat		= 0,
-	.mif3_mode	= 3,
-	.rst_to_cke	= 0x23,
-	.sde_to_rst	= 0x10,
-	.refsel = 1,
-	.refr = 3,
-};
-
-static struct mx6_ddr3_cfg mem_ddr = {
-	.mem_speed = 1333,
-	.density = 2,
-	.width = 16,
-	.banks = 8,
-	.coladdr = 10,
-	.pagesz = 2,
-	.trcd = 1350,
-	.trcmin = 4950,
-	.trasmin = 3600,
-};
-
+/* clock control general registers */
 static void ccgr_init(void)
 {
 	struct mxc_ccm_reg *ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
-
+	/* imx6ul */
 	writel(0xFFFFFFFF, &ccm->CCGR0);
 	writel(0xFFFFFFFF, &ccm->CCGR1);
 	writel(0xFFFFFFFF, &ccm->CCGR2);
@@ -105,45 +57,173 @@ static void ccgr_init(void)
 	writel(0xFFFFFFFF, &ccm->CCGR4);
 	writel(0xFFFFFFFF, &ccm->CCGR5);
 	writel(0xFFFFFFFF, &ccm->CCGR6);
+	writel(0xFFFFFFFF, &ccm->CCGR7);
 }
 
-static void imx6ul_spl_dram_cfg_size(u32 ram_size)
+static void ddr3_512mb_init(void)
 {
-	if (ram_size == SZ_256M)
-		mem_ddr.rowaddr = 14;
-	else
-		mem_ddr.rowaddr = 15;
+	writel(0xFFFFFFFF, 0x020C4080);
+	writel(0x000C0000, 0x020E04B4);
+	writel(0x00000000, 0x020E04AC);
+	writel(0x00000030, 0x020E027C);
+	writel(0x00000030, 0x020E0250);
+	writel(0x00000030, 0x020E024C);
+	writel(0x00000030, 0x020E0490);
+	writel(0x00000030, 0x020E0288);
+	writel(0x00000000, 0x020E0270);
+	writel(0x00000030, 0x020E0260);
+	writel(0x00000030, 0x020E0264);
+	writel(0x00000030, 0x020E04A0);
+	writel(0x00020000, 0x020E0494);
+	writel(0x00000030, 0x020E0280);
+	writel(0x00000030, 0x020E0284);
+	writel(0x00020000, 0x020E04B0);
+	writel(0x00000030, 0x020E0498);
+	writel(0x00000030, 0x020E04A4);
+	writel(0x00000030, 0x020E0244);
+	writel(0x00000030, 0x020E0248);
+	writel(0xA1390003, 0x021B0800);
+	writel(0x00000000, 0x021B080C);
+//	writel(0x001F001F ,0x021b0810);
+	writel(0x41440140, 0x021B083C);
+	writel(0x00000000, 0x021b0840);
+	writel(0x40404246, 0x021B0848);
+	writel(0x40405048, 0x021B0850);
+	writel(0x33333333, 0x021B081C);
+	writel(0x33333333, 0x021B0820);
+	writel(0xf3333333, 0x021B082C);
+	writel(0xf3333333, 0x021B0830);
+	writel(0x00921012, 0x021B08C0);
+	writel(0x00000800, 0x021B08B8);
+	writel(0x0002002D, 0x021B0004);
+	writel(0x1B333030, 0x021B0008);
+	writel(0x676B52F3, 0x021B000C);
+	writel(0xB66D0B63, 0x021B0010);
+	writel(0x01FF00DB, 0x021B0014);
+	writel(0x00201740, 0x021B0018);
+	writel(0x000026D2, 0x021B002C);
+	writel(0x006B1023, 0x021B0030);
+	writel(0x0000004F, 0x021B0040);
+	writel(0x84180000, 0x021B0000);
 
-	mx6ul_dram_iocfg(mem_ddr.width, &mx6_ddr_ioregs, &mx6_grp_ioregs);
-	mx6_dram_cfg(&ddr_sysinfo, &mx6_mmcd_calib, &mem_ddr);
+	writel(0x02008032, 0x021B001C);
+	writel(0x00008033, 0x021B001C);
+	writel(0x00048031, 0x021B001C);
+	writel(0x15208030, 0x021B001C);
+	writel(0x04008040, 0x021B001C);
+
+	writel(0x00000800, 0x021B0020);
+	writel(0x00000227, 0x021B0818);
+	writel(0x0002556D, 0x021B0004);
+	writel(0x00011006, 0x021B0404);
+
+	writel(0x00000000, 0x021B001C);
+
 }
 
-static void imx6ul_spl_dram_cfg(void)
+static void ddr3_256mb_init(void)
 {
-	ulong ram_size_test, ram_size = 0;
+	writel(0xFFFFFFFF, 0x020C4080);
+	writel(0x000C0000, 0x020E04B4);
+	writel(0x00000000, 0x020E04AC);
+	writel(0x00000030, 0x020E027C);
+	writel(0x00000030, 0x020E0250);
+	writel(0x00000030, 0x020E024C);
+	writel(0x00000030, 0x020E0490);
+	writel(0x00000030, 0x020E0288);
+	writel(0x00000000, 0x020E0270);
+	writel(0x00000030, 0x020E0260);
+	writel(0x00000030, 0x020E0264);
+	writel(0x00000030, 0x020E04A0);
+	writel(0x00020000, 0x020E0494);
+	writel(0x00000030, 0x020E0280);
+	writel(0x00000030, 0x020E0284);
+	writel(0x00020000, 0x020E04B0);
+	writel(0x00000030, 0x020E0498);
+	writel(0x00000030, 0x020E04A4);
+	writel(0x00000030, 0x020E0244);
+	writel(0x00000030, 0x020E0248);
+	writel(0x00008000, 0x021B001C);
+	writel(0xA1390003, 0x021B0800);
+	writel(0x00000000, 0x021B080C);
+	writel(0x01380134, 0x021B083C);
+	writel(0x40404244, 0x021B0848);
+	writel(0x40405050, 0x021B0850);
+	writel(0x33333333, 0x021B081C);
+	writel(0x33333333, 0x021B0820);
+	writel(0xf3333333, 0x021B082C);
+	writel(0xf3333333, 0x021B0830);
+	writel(0x00921012, 0x021B08C0);
+	writel(0x00000800, 0x021B08B8);
+	writel(0x0002002D, 0x021B0004);
+	writel(0x00333030, 0x021B0008);
+	writel(0x676B52F3, 0x021B000C);
+	writel(0xB66D8B63, 0x021B0010);
+	writel(0x01FF00DB, 0x021B0014);
+	writel(0x00201740, 0x021B0018);
+	writel(0x00008000, 0x021B001C);
+	writel(0x000026D2, 0x021B002C);
+	writel(0x006B1023, 0x021B0030);
+	writel(0x00000047, 0x021B0040);
+	writel(0x83180000, 0x021B0000);
+	writel(0x02008032, 0x021B001C);
+	writel(0x00008033, 0x021B001C);
+	writel(0x00048031, 0x021B001C);
+	writel(0x15208030, 0x021B001C);
+	writel(0x04008040, 0x021B001C);
+	writel(0x00000800, 0x021B0020);
+	writel(0x00000227, 0x021B0818);
+	writel(0x0002552D, 0x021B0004);
+	writel(0x00011006, 0x021B0404);
+	writel(0x00000000, 0x021B001C);
+}
 
-	for (ram_size = SZ_512M; ram_size >= SZ_256M; ram_size >>= 1) {
-		imx6ul_spl_dram_cfg_size(ram_size);
-		ram_size_test = get_ram_size((long int *)PHYS_SDRAM, ram_size);
-		if (ram_size_test == ram_size)
-			break;
-	}
+static void spl_dram_init(void)
+{
+	setup_iomux_ddr_type_detection();
+	gpio_direction_input(DDR_TYPE_DET);
 
-	if (ram_size < SZ_256M) {
-		puts("ERROR: DRAM size detection failed\n");
-		hang();
+	if (!gpio_get_value(DDR_TYPE_DET)) {
+		ddr3_512mb_init();
+	} else {
+		ddr3_256mb_init();
 	}
 }
+
+#if 0 //the instance where in soc.c
+/* General Purpose Registers */
+void gpr_init(void)
+{
+	struct iomuxc_gpr_base_regs *gpr_regs = (struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+	writel(0x4F400005, &gpr_regs->gpr[1]);
+}
+#endif
 
 void board_init_f(ulong dummy)
 {
-	ccgr_init();
+	/* setup AIPS and disable watchdog */
 	arch_cpu_init();
+
+	ccgr_init();
+
+	gpr_init();
+
+	/* iomux and setup of i2c */
 	board_early_init_f();
+
+	/* setup GP timer */
 	timer_init();
+
+	/* UART clocks enabled and gd valid - init serial console */
 	preloader_console_init();
-	imx6ul_spl_dram_cfg();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	/* Clear the BSS. */
 	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	/* load/boot image from boot device */
 	board_init_r(NULL, 0);
 }
 
@@ -151,8 +231,8 @@ void reset_cpu(ulong addr)
 {
 }
 
-#define USDHC_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |		\
-	PAD_CTL_PUS_22K_UP  | PAD_CTL_SPEED_LOW |		\
+#define USDHC_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |            \
+	PAD_CTL_PUS_22K_UP  | PAD_CTL_SPEED_LOW |               \
 	PAD_CTL_DSE_80ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
 
 static iomux_v3_cfg_t const usdhc1_pads[] = {
@@ -183,4 +263,4 @@ int board_mmc_init(struct bd_info *bis)
 	usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
 	return fsl_esdhc_initialize(bis, &usdhc_cfg[0]);
 }
-#endif
+#endif /* CONFIG_SPL_BUILD */
