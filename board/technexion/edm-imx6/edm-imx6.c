8b18048bd8d571585b269c74279031bcc4f65dbe
Joe Zhang <joe.zhang@technexion.com>
2022-04-26 11:38:57 +0800

    edm-imx6: mmc: workaround: boot time is too slow
    The mmc is trying to apply the max speed.
    But somehow it failed, and trying to get next slower speed.
    Until it reach the speed for 1-bit.
    
    tep1-imx7d shares the same issue.
    
    We set the max speed to 1-bit for workaround.

--- board/technexion/edm-imx6/edm-imx6.c
+++ board/technexion/edm-imx6/edm-imx6.c
@@ -217,7 +217,7 @@ int board_mmc_init(struct bd_info *bis)
 		default:
 			usdhc_cfg[0].esdhc_base = USDHC3_BASE_ADDR;
 			usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
-			usdhc_cfg[0].max_bus_width = 4;
+			usdhc_cfg[0].max_bus_width = 1;
 			usdhc_cfg[1].esdhc_base = USDHC1_BASE_ADDR;
 			usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
 			usdhc_cfg[1].max_bus_width = 4;
