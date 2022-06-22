ab3414e6e855eb753da8927a90c05a2e528c9885
Ray Chang <ray.chang@technexion.com>
2022-06-22 16:52:26 +0800

    edm-g-imx8mm: set the iomux to open-drain mode
    Remove the iomux from the board file only to keep the device tree.
    Incorrect iomux settings can cause Ethernet phy to fail to initialize
    on some boards.

--- board/technexion/edm-g-imx8mm/edm-g-imx8mm.c
+++ board/technexion/edm-g-imx8mm/edm-g-imx8mm.c
@@ -107,40 +107,11 @@ ulong board_get_usable_ram_top(ulong total_size)
 }
 
 #if IS_ENABLED(CONFIG_FEC_MXC)
-#define FEC_RST_PAD IMX_GPIO_NR(2, 7)
-static iomux_v3_cfg_t const fec1_rst_pads[] = {
-	IMX8MM_PAD_SD1_DATA5_GPIO2_IO7 | MUX_PAD_CTRL(NO_PAD_CTRL),
-};
-
-#define PHY_PWR_PAD IMX_GPIO_NR(4, 0)
-static iomux_v3_cfg_t const phy_pwr_pads[] = {
-	IMX8MM_PAD_SAI1_RXFS_GPIO4_IO0 | MUX_PAD_CTRL(NO_PAD_CTRL),
-};
-
-static void setup_iomux_fec(void)
-{
-	imx_iomux_v3_setup_multiple_pads(fec1_rst_pads,
-					 ARRAY_SIZE(fec1_rst_pads));
-
-	imx_iomux_v3_setup_multiple_pads(phy_pwr_pads,
-					 ARRAY_SIZE(phy_pwr_pads));
-
-	gpio_request(PHY_PWR_PAD, "phy_pwr");
-	gpio_direction_output(PHY_PWR_PAD, 1);
-	mdelay(100);
-
-	gpio_request(FEC_RST_PAD, "fec1_rst");
-	gpio_direction_output(FEC_RST_PAD, 0);
-	udelay(40000);
-	gpio_direction_output(FEC_RST_PAD, 1);
-}
-
 static int setup_fec(void)
 {
 	struct iomuxc_gpr_base_regs *gpr =
 		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
 
-	setup_iomux_fec();
 	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
 	clrsetbits_le32(&gpr->gpr[1], 0x2000, 0);
 
