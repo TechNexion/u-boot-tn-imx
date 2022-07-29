eee6f9834b5abe1bd22a8562ce731af5a4f6fed5
Joe Zhang <joe.zhang@technexion.com>
2022-07-29 11:35:23 +0800

    wandboard: disable DM_I2C & DM_PMIC

--- include/configs/wandboard.h
+++ include/configs/wandboard.h
@@ -28,6 +28,23 @@
 #define CONFIG_LBA48
 #endif
 
+/* I2C Configs */
+#define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC
+#define CONFIG_SYS_I2C_MXC_I2C1         /* enable I2C bus 1 */
+#define CONFIG_SYS_I2C_MXC_I2C2         /* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_MXC_I2C3         /* enable I2C bus 3 */
+#define CONFIG_SYS_I2C_SPEED            100000
+#define CONFIG_I2C_EDID
+#define I2C_PMIC_BUS			2
+
+/* PMIC */
+#define CONFIG_POWER
+#define CONFIG_POWER_I2C
+#define CONFIG_POWER_PFUZE100
+#define CONFIG_POWER_PFUZE100_I2C_ADDR	0x08
+#define CONFIG_SYS_FSL_PMIC_I2C_ADDR	0x08
+
 /* MMC Configuration */
 #define CONFIG_SYS_FSL_USDHC_NUM	2
 #define CONFIG_SYS_FSL_ESDHC_ADDR	0
