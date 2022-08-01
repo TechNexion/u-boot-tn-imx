6876aef7b7f8d60ade5ec86c58ba9b6e5148d709
Joe Zhang <joe.zhang@technexion.com>
2022-08-01 10:25:54 +0800

    tep5-imx6: mmc: workaround: boot time is too slow
    The mmc is trying to apply the max speed.
    But somehow it failed, and trying to get next slower speed.
    Until it reach the speed for 1-bit.
    
    We set the max speed to 1-bit for workaround.

--- board/technexion/tek-imx6/tek-imx6.c
+++ board/technexion/tek-imx6/tek-imx6.c
@@ -207,13 +207,13 @@ int board_mmc_init(struct bd_info *bis)
 		usdhc_cfg[0].max_bus_width = 4;
 		usdhc_cfg[1].esdhc_base = USDHC3_BASE_ADDR;
 		usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
-		usdhc_cfg[1].max_bus_width = 4;
+		usdhc_cfg[1].max_bus_width = 1;
 		break;
 	case SD3_BOOT:
 	default:
 		usdhc_cfg[0].esdhc_base = USDHC3_BASE_ADDR;
 		usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
-		usdhc_cfg[0].max_bus_width = 4;
+		usdhc_cfg[0].max_bus_width = 1;
 		usdhc_cfg[1].esdhc_base = USDHC1_BASE_ADDR;
 		usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
 		usdhc_cfg[1].max_bus_width = 4;
