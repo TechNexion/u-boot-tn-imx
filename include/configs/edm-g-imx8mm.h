161fe8e86ab2c45632e38d3cd4b34014b53e1496
Joe Zhang <joe.zhang@technexion.com>
2022-10-11 15:14:38 +0800

    edm-g-imx8mm: add fdt_buffer
    This is part of this commit:
    2da1bd617e ("edm-g-imx8mm/ flex-imx8mm/ pico-imx8mm/ tek-imx8mp: add fdt_buffer", 2021-12-16)

--- include/configs/edm-g-imx8mm.h
+++ include/configs/edm-g-imx8mm.h
@@ -100,6 +100,7 @@
 	"fdt_addr_r=0x43000000\0"			\
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0" \
+	"fdt_buffer=8192\0"	\
 	"mtdparts=" MFG_NAND_PARTITION "\0" \
 	"console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200\0" \
 	"bootargs=console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200 ubi.mtd=nandrootfs "  \
