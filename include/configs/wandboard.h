cfe1eb885d5741e989a2f68aada9a56b245884c3
Sean Xie <sean.xie@technexion.com>
2022-11-01 13:20:23 +0800

    wandboard-imx6: modify configs and make u-boot can load the Linux kernel image

--- include/configs/wandboard.h
+++ include/configs/wandboard.h
@@ -10,20 +10,55 @@
 
 #include "mx6_common.h"
 
-#define CFG_MXC_UART_BASE		UART1_BASE
+#include "imx6_spl.h"
+
+#define CONFIG_MACH_TYPE			MACH_TYPE_WANDBOARD_IMX6
+
+#define CONFIG_MXC_UART_BASE		UART1_BASE
+
+#ifndef CONFIG_SILENT_CONSOLE
+#define SILENT_ENABLE			0
+#define DEBUG_TTY			ttymxc0
+#else
+#define SILENT_ENABLE			1
+#define DEBUG_TTY			null
+#endif
+
+/* SATA Configs */
+
+#ifdef CONFIG_CMD_SATA
+#define CONFIG_DWC_AHSATA_PORT_ID	0
+#define CONFIG_DWC_AHSATA_BASE_ADDR	SATA_ARB_BASE_ADDR
+#define CONFIG_LBA48
+#endif
 
 /* MMC Configuration */
-#define CFG_SYS_FSL_USDHC_NUM	2
-#define CFG_SYS_FSL_ESDHC_ADDR	0
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_SYS_FSL_ESDHC_ADDR	0
 
 /* USB Configs */
-#define CFG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CFG_MXC_USB_FLAGS		0
+#define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS		0
 
-#define CFG_EXTRA_ENV_SETTINGS \
+/* Framebuffer */
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_IMX_HDMI
+#define CONFIG_IMX_VIDEO_SKIP
+
+#define CONFIG_DFU_ENV_SETTINGS \
+	"dfu_alt_info=" \
+		"spl raw 0x2 0x400;" \
+		"u-boot raw 0x8a 0x1000;" \
+		"/boot/zImage ext4 0 1;" \
+		"rootfs part 0 1\0" \
+
+#if (defined(CONFIG_DISTRO_DEFAULTS))
+#define CONFIG_EXTRA_ENV_SETTINGS \
 	"console=ttymxc0\0" \
 	"splashpos=m,m\0" \
 	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"baseboard=wandboard\0" \
+	"default_baseboard=wandboard\0" \
 	"fdtfile=undefined\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
@@ -32,6 +67,10 @@
 	"ip_dyn=yes\0" \
 	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
 	"finduuid=part uuid mmc 0:1 uuid\0" \
+	"mmcautodetect=yes\0" \
+	"fdtfile_autodetect=on\0" \
+	"bootdev_autodetect=on\0" \
+	"display_autodetect=on\0" \
 	"update_sd_firmware_filename=u-boot.imx\0" \
 	"update_sd_firmware=" \
 		"if test ${ip_dyn} = yes; then " \
@@ -81,13 +120,161 @@
 
 #include <config_distro_bootcmd.h>
 #include <linux/stringify.h>
+#else
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"pxefile_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"ramdisk_addr_r=0x13000000\0" \
+	"ramdiskaddr=0x13000000\0" \
+	"scriptaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"script=boot.scr\0" \
+	"image=zImage\0" \
+	"silent=" __stringify(SILENT_ENABLE) "\0" \
+	"console=" __stringify(DEBUG_TTY)"\0" \
+	"splashpos=m,m\0" \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"baseboard=wandboard\0" \
+	"default_baseboard=wandboard\0" \
+	"fdtfile=undefined\0" \
+	"fdt_high=0xffffffff\0" \
+	"initrd_high=0xffffffff\0" \
+	"fdt_addr=0x18000000\0" \
+	"boot_fdt=try\0" \
+	"ip_dyn=yes\0" \
+	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
+	"mmcpart=1\0" \
+	"mmcautodetect=yes\0" \
+	"searchbootdev=" \
+		"if test ${bootdev} = MMC3; then " \
+			"setenv mmcrootdev /dev/mmcblk2; " \
+			"setenv mmcroot /dev/mmcblk2p2 rootwait rw; " \
+		"elif test ${bootdev} = SD1; then; " \
+			"setenv mmcrootdev /dev/mmcblk0; " \
+			"setenv mmcroot /dev/mmcblk0p2 rootwait rw; " \
+		"elif test ${bootdev} = SATA; then; " \
+			"setenv mmcrootdev /dev/sda; " \
+			"setenv mmcroot /dev/sda2 rootwait rw; " \
+		"fi\0" \
+	"mmcargs=setenv bootargs console=${console},${baudrate} " \
+		"root=${mmcroot}\0" \
+	"fdtfile_autodetect=on\0" \
+	"bootdev_autodetect=on\0" \
+	"display_autodetect=on\0" \
+	"update_sd_firmware_filename=u-boot.imx\0" \
+	"update_sd_firmware=" \
+		"if test ${ip_dyn} = yes; then " \
+			"setenv get_cmd dhcp; " \
+		"else " \
+			"setenv get_cmd tftp; " \
+		"fi; " \
+		"if mmc dev ${mmcdev}; then "	\
+			"if ${get_cmd} ${update_sd_firmware_filename}; then " \
+				"setexpr fw_sz ${filesize} / 0x200; " \
+				"setexpr fw_sz ${fw_sz} + 1; "	\
+				"mmc write ${loadaddr} 0x2 ${fw_sz}; " \
+			"fi; "	\
+		"fi\0" \
+	"loadbootscript=" \
+		"fatload ${bootmedia} ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
+	"bootscript=echo Running bootscript from ${bootmedia} ...; " \
+		"source\0" \
+	"loadimage=fatload ${bootmedia} ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
+	"loadfdt=fatload ${bootmedia} ${mmcdev}:${mmcpart} ${fdt_addr} ${fdtfile}\0" \
+	"mmcboot=echo Booting from ${bootmedia} ...; " \
+		"run searchbootdev; " \
+		"run mmcargs; " \
+		"echo baseboard is ${baseboard}; " \
+		"echo ${bootargs}; " \
+		"run setfdt; " \
+		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
+			"if run loadfdt; then " \
+				"bootz ${loadaddr} - ${fdt_addr}; " \
+			"else " \
+				"if test ${boot_fdt} = try; then " \
+					"echo WARN: Cannot load the DT; " \
+					"echo fall back to load the default DT; " \
+					"setenv baseboard ${default_baseboard}; " \
+					"run setfdt; " \
+					"run loadfdt; " \
+					"bootz ${loadaddr} - ${fdt_addr}; " \
+				"else " \
+					"echo WARN: Cannot load the DT; " \
+				"fi; " \
+			"fi; " \
+		"else " \
+			"bootz; " \
+		"fi;\0" \
+	"bootenv=uEnv.txt\0" \
+	"loadbootenv=fatload ${bootmedia} ${mmcdev} ${loadaddr} ${bootenv}\0" \
+	"importbootenv=echo Importing environment from ${bootmedia} ...; " \
+		"env import -t -r $loadaddr $filesize\0" \
+	"fitov=\"\"\0" \
+	"fit_addr=0x21100000\0" \
+	"fit_high=0xffffffff\0" \
+	"fit_overlay=for ov in ${dtoverlay}; do " \
+			"echo Overlaying ${ov}...; setenv fitov \"${fitov}#${ov}\"; " \
+		"done; echo fit conf: ${fdtfile}${fitov};\0" \
+	"fitargs=setenv bootargs console=${console},${baudrate} root=/dev/ram0 rootwait rw " \
+		"modules-load=g_acm_ms g_acm_ms.stall=0 g_acm_ms.removable=1 g_acm_ms.file=${mmcrootdev} " \
+		"g_acm_ms.iSerialNumber=00:00:00:00:00:00 g_acm_ms.iManufacturer=TechNexion\0" \
+	"loadfit=fatload mmc ${mmcdev} ${fit_addr} tnrescue.itb\0" \
+	"fitboot=echo Booting from FIT image...; " \
+		"run searchbootdev; run setfdt; run fit_overlay; run fitargs; " \
+		"bootm ${fit_addr}#conf@${fdtfile}${fitov};\0" \
+	"setfdt="\
+		"if test $board_name = D1 && test $board_rev = MX6QP ; then " \
+			"setenv fdtfile imx6qp-wandboard-revd1.dtb; fi; " \
+		"if test $board_name = D1 && test $board_rev = MX6Q ; then " \
+			"setenv fdtfile imx6q-wandboard-revd1.dtb; fi; " \
+		"if test $board_name = D1 && test $board_rev = MX6DL ; then " \
+			"setenv fdtfile imx6dl-wandboard-revd1.dtb; fi; " \
+		"if test $board_name = C1 && test $board_rev = MX6Q ; then " \
+			"setenv fdtfile imx6q-wandboard.dtb; fi; " \
+		"if test $board_name = C1 && test $board_rev = MX6DL ; then " \
+			"setenv fdtfile imx6dl-wandboard.dtb; fi; " \
+		"if test $board_name = B1 && test $board_rev = MX6Q ; then " \
+			"setenv fdtfile imx6q-wandboard-revb1.dtb; fi; " \
+		"if test $board_name = B1 && test $board_rev = MX6DL ; then " \
+			"setenv fdtfile imx6dl-wandboard-revb1.dtb; fi; " \
+		"if test $fdtfile = undefined; then " \
+			"echo WARNING: Could not determine dtb to use; fi; \0"
+
+#define CONFIG_BOOTCOMMAND \
+	   "mmc dev ${mmcdev}; if mmc rescan; then " \
+		   "run searchbootdev; " \
+		   "if run loadbootenv; then " \
+			   "echo Loaded environment from ${bootenv};" \
+			   "run importbootenv;" \
+		   "fi;" \
+		   "if test -n $uenvcmd; then " \
+			   "echo Running uenvcmd ...;" \
+			   "run uenvcmd;" \
+		   "fi;" \
+		   "if run loadbootscript; then " \
+			   "run bootscript; " \
+		   "fi; " \
+		   "if run loadfit; then " \
+			   "run fitboot; " \
+		   "fi; " \
+		   "if run loadimage; then " \
+			   "run mmcboot; " \
+		   "else " \
+			   "echo WARN: Cannot load kernel from boot media; " \
+		   "fi; " \
+	   "else run netboot; fi"
+#endif
 
 /* Physical Memory Map */
 #define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
 
-#define CFG_SYS_SDRAM_BASE		PHYS_SDRAM
-#define CFG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
-#define CFG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
 
 /* Environment organization */
 
