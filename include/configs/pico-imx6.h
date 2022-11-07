fd0ada26c704ed90d1fdf40a3e3581528dce07d7
Sean Xie <sean.xie@technexion.com>
2022-11-07 15:28:51 +0800

    pico-imx6: make the u-boot splash screen work

--- include/configs/pico-imx6.h
+++ include/configs/pico-imx6.h
@@ -67,6 +67,7 @@
 	"console=ttymxc0\0" \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	BOOTMENU_ENV \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"splashpos=m,m\0" \
 	"splashsource=mmc_fs\0" \
 	"som=autodetect\0" \
