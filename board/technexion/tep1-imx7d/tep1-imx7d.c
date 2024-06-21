92dd9dea4af072f3c31cee82c6bd4bb05374b68e
Joe Zhang <joe.zhang@technexion.com>
2024-06-21 11:30:49 +0800

    tep1-imx7d: bring back the splash screen
    video: Rename CONFIG_DM_VIDEO to CONFIG_VIDEO
    
    Now that all the old code is gone, rename this option. Driver model
    migration is now complete.

--- board/technexion/tep1-imx7d/tep1-imx7d.c
+++ board/technexion/tep1-imx7d/tep1-imx7d.c
@@ -40,7 +40,6 @@
 
 #ifdef CONFIG_VIDEO_MXS
 #include <linux/fb.h>
-#include <mxsfb.h>
 #include <splash.h>
 #endif
 
@@ -523,7 +522,7 @@ int board_early_init_f(void)
 	return 0;
 }
 
-#ifdef CONFIG_DM_VIDEO
+#ifdef CONFIG_VIDEO
 void setup_lcd(void)
 {
 	/* Set Brightness to high */
@@ -555,7 +554,7 @@ int board_init(void)
 		imx_iomux_v3_setup_multiple_pads(uart2_pads, ARRAY_SIZE(uart2_pads));
 	}
 
-#ifdef CONFIG_DM_VIDEO
+#ifdef CONFIG_VIDEO
 	setup_lcd();
 #endif
 #ifdef CONFIG_FEC_MXC
