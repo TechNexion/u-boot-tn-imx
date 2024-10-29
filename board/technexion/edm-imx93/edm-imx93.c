a28a9d79b7c4503cb736c65a6fc52cb5995871dd
Ray Chang <ray.chang@technexion.com>
2024-10-29 13:33:44 +0800

    edm-imx93: add splash screen support for 5/7 inch panel

--- board/technexion/edm-imx93/edm-imx93.c
+++ board/technexion/edm-imx93/edm-imx93.c
@@ -24,6 +24,10 @@
 #include <asm/gpio.h>
 #include <mmc.h>
 #include "../common/periph_detect.h"
+#include <splash.h>
+#include <imx_sip.h>
+#include <linux/arm-smccc.h>
+#include <cli.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -92,6 +96,53 @@ static int setup_eqos(void)
 {
 	return 0;
 }
+
+#ifdef CONFIG_SPLASH_SCREEN
+static struct splash_location imx_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x100000,
+	},
+	{
+		.name = "mmc_fs",
+		.storage = SPLASH_STORAGE_MMC,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "usb_fs",
+		.storage = SPLASH_STORAGE_USB,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "sata_fs",
+		.storage = SPLASH_STORAGE_SATA,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+};
+
+/*This function is defined in common/splash.c.
+  Declare here to remove warning. */
+int splash_video_logo_load(void);
+
+int splash_screen_prepare(void)
+{
+	imx_splash_locations[1].devpart[0] = mmc_get_env_dev() + '0';
+	int ret;
+	ret = splash_source_load(imx_splash_locations, ARRAY_SIZE(imx_splash_locations));
+	if (!ret)
+		return 0;
+	else {
+		printf("\nNo splash.bmp in boot partition!!\n");
+		printf("Using default logo!!\n\n");
+		return splash_video_logo_load();
+	}
+}
+#endif /* CONFIG_SPLASH_SCREEN */
 
 static int check_mmc_autodetect(void)
 {
