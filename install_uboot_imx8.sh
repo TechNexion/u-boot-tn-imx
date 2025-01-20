524c4a54185eb7b22106a0ee678ec9862920923b
Joe Zhang <joe.zhang@technexion.com>
2025-01-20 16:21:59 +0800

    edm-imx91: add initial support

--- install_uboot_imx8.sh
+++ install_uboot_imx8.sh
@@ -316,7 +316,10 @@ usage()
     ./install_uboot_imx8.sh -b imx93-pico.dtb -d /dev/sdX
 
     * AXON-IMX91:
-    ./install_uboot_imx8.sh -b imx91-axon.dtb -d /dev/sdf
+    ./install_uboot_imx8.sh -b imx91-axon.dtb -d /dev/sdX
+
+    * EDM-IMX91:
+    ./install_uboot_imx8.sh -b imx91-edm.dtb -d /dev/sdX
 "
 }
 
