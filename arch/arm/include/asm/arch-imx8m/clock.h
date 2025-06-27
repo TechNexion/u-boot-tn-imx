39e665fa5735d1b1e8be56fc805ba4ff50db57e3
Ray Chang <ray.chang@technexion.com>
2025-06-27 17:51:55 +0800

    imx8m: clock: add set_clk_eqos
    This function is not defined in the header file.

--- arch/arm/include/asm/arch-imx8m/clock.h
+++ arch/arm/include/asm/arch-imx8m/clock.h
@@ -280,6 +280,7 @@ void init_clk_fspi(int index);
 void enable_ocotp_clk(unsigned char enable);
 int enable_i2c_clk(unsigned char enable, unsigned int i2c_num);
 int set_clk_enet(enum enet_freq type);
+int set_clk_eqos(enum enet_freq type);
 void hab_caam_clock_enable(unsigned char enable);
 void enable_usboh3_clk(unsigned char enable);
 u32 get_dsi_phy_ref_clk(void);
