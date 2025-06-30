45ed151ec2f6dfe1095fe31e3350a08e304c8ced
Ray Chang <ray.chang@technexion.com>
2025-06-30 13:13:03 +0800

    pico-imx8mm: remove 3GB memory sku

--- board/technexion/pico-imx8mm/pico-imx8mm_spl.c
+++ board/technexion/pico-imx8mm/pico-imx8mm_spl.c
@@ -83,7 +83,6 @@ static void setup_iomux_ver_det(void)
 /***********************************************
 BOARD_ID0    BOARD_ID1   BOARD_ID2
    0            0            1       4G LPDDR4
-   1            1            1       3G LPDDR4
    1            1            0       2G LPDDR4
    1            0            1       1G LPDDR4
 ************************************************/
@@ -103,11 +102,6 @@ void spl_dram_init(void)
 		puts("dram_init: LPDDR4 4GB\n");
 		ddr_init(&dram_timing_4gb);
 		writel(0x4, MCU_BOOTROM_BASE_ADDR);
-	}
-	else if (gpio_get_value(BOARD_ID0) && gpio_get_value(BOARD_ID1) && gpio_get_value(BOARD_ID2)) {
-		puts("dram_init: LPDDR4 3GB\n");
-		ddr_init(&dram_timing_3gb);
-		writel(0x3, MCU_BOOTROM_BASE_ADDR);
 	}
 	else if (gpio_get_value(BOARD_ID0) && gpio_get_value(BOARD_ID1) && !gpio_get_value(BOARD_ID2)) {
 		puts("dram_init: LPDDR4: 2GB\n");
