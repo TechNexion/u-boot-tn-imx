5a6875df5b4305041a28b69d6525859c3f822862
Richard Hu <richard.hu@technexion.com>
2025-07-02 14:32:54 +0800

    axon-imx8mp: avoid using global variables during board_phys_sdram_size() stage
    The static variable declaration:
    static u8 ddr_code __section("data"); does not work as intended.
    It should be corrected to
    static u8 ddr_code __section(".data");
    
    To prevent similar issues in the future, we are avoiding the use of global variables in this context.

--- board/technexion/axon-imx8mp/axon-imx8mp.c
+++ board/technexion/axon-imx8mp/axon-imx8mp.c
@@ -77,9 +77,9 @@ size_t tn_display_count = ARRAY_SIZE(displays);
 
 static u8 ddr_code __section("data");
 
-static void board_get_ddr_code(void)
+static u8 board_get_ddr_code(void)
 {
-	ddr_code = readl(OCRAM_BASE_ADDR);
+	return (readl(OCRAM_BASE_ADDR));
 }
 
 int board_phys_sdram_size(phys_size_t *size)
@@ -87,7 +87,7 @@ int board_phys_sdram_size(phys_size_t *size)
 	if (!size)
 		return -EINVAL;
 
-	switch (ddr_code) {
+	switch (board_get_ddr_code()) {
 	case LPDDR4_8GB:
 		*size = SZ_8G;
 		break;
@@ -123,9 +123,6 @@ int board_early_init_f(void)
 
 	init_uart_clk(1);
 
-#ifndef CONFIG_SPL_BUILD
-	board_get_ddr_code();
-#endif
 	return 0;
 }
 
