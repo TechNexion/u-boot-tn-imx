fb8f986c296b7f265a3a79859fce111251d6857d
Richard Hu <richard.hu@technexion.com>
2025-07-03 11:13:54 +0800

    edm-g-imx8mp: fix: no console output after enabling CONFIG_DM_SERIAL

--- board/technexion/edm-g-imx8mp/edm-g-imx8mp_spl.c
+++ board/technexion/edm-g-imx8mp/edm-g-imx8mp_spl.c
@@ -225,8 +225,6 @@ void board_init_f(ulong dummy)
 
 	timer_init();
 
-	preloader_console_init();
-
 	ret = spl_early_init();
 	if (ret) {
 		debug("spl_early_init() failed: %d\n", ret);
@@ -240,6 +238,8 @@ void board_init_f(ulong dummy)
 		printf("Failed to find clock node. Check device tree\n");
 		hang();
 	}
+
+	preloader_console_init();
 
 	enable_tzc380();
 
