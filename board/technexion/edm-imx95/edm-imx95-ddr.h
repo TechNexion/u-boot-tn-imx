2ca46bc91c5a352c16062e52f161861bb2e62905
Richard Hu <richard.hu@technexion.com>
2025-09-18 16:40:58 +0800

    edm-imx95: determine DRAM size dynamically from the information passed by imx-oei

---
+++ board/technexion/edm-imx95/edm-imx95-ddr.h
@@ -0,0 +1,26 @@
+ // SPDX-License-Identifier: GPL-2.0+
+ /*
+  * Copyright 2025 TechNexion Ltd.
+  *
+  * Author: Richard Hu <richard.hu@technexion.com>
+  *
+  */
+#ifndef __EDM_IMX95_DDR_H
+#define __EDM_IMX95_DDR_H
+
+/***********************************************
+ * BOARD_ID1   BOARD_ID0
+ *     1            1       16G LPDDR5
+ *     1            0       8G LPDDR5
+ *     0            1       4G LPDDR5 
+************************************************/
+enum {
+	LPDDR5_16GB = 0x3,
+	LPDDR5_8GB = 0x2,
+	LPDDR5_4GB = 0x1,
+	LPDDR5_UNKNOWN = 0xf,
+};
+
+#define OCRAM_NON_SECURE_BASE_ADDR 0x0204C0000
+
+#endif /* __EDM_IMX95_DDR_H */
