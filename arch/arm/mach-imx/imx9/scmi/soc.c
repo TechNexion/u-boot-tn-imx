f367df11d4e83c9cd2e20c6d87367cd0e86efee0
Ray Chang <ray.chang@technexion.com>
2025-10-17 13:17:49 +0800

    imx9: scmi: conditionally compile cache-enabling code
    The functions for enabling caches in SPL should only be compiled
    when secure SDRAM is configured. This commit wraps the relevant
    function calls and definitions with the CFG_SYS_SECURE_SDRAM_SIZE
    macro to prevent compilation errors in other board configurations.

--- arch/arm/mach-imx/imx9/scmi/soc.c
+++ arch/arm/mach-imx/imx9/scmi/soc.c
@@ -515,6 +515,7 @@ void enable_caches(void)
 	dcache_enable();
 }
 
+#if defined(CFG_SYS_SECURE_SDRAM_SIZE)
 /*
  * Initialize the MMU and activate cache in SPL stage
  */
@@ -542,6 +543,7 @@ void spl_board_prepare_for_boot(void)
 {
 	dcache_disable();
 }
+#endif
 
 __weak int board_phys_sdram_size(phys_size_t *size)
 {
@@ -1656,7 +1658,9 @@ int arch_cpu_init(void)
 		gpio_reset(GPIO7_BASE_ADDR);
 #endif
 
+#if defined(CFG_SYS_SECURE_SDRAM_SIZE)
 		spl_enable_caches();
+#endif
 	}
 
 	return 0;
