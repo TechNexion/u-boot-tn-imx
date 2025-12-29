acf05b014fee3f6b87f574f2f68bd1cd6e03476d
Ray Chang <ray.chang@technexion.com>
2025-12-29 13:19:47 +0800

    install_uboot_imx8.sh: update imx-mkimage, imx-atf, and firmware to match kernel lf-6.12.49_2.2.0

--- install_uboot_imx8.sh
+++ install_uboot_imx8.sh
@@ -11,12 +11,12 @@
 
 DRIVE=/dev/sdX
 
-BRANCH_VER="lf-6.12.34_2.1.0" #branch used by imx-mkimage and imx-atf under meta-imx
+BRANCH_VER="lf-6.12.49_2.2.0" #branch used by imx-mkimage and imx-atf under meta-imx
 ATF_BRANCH_VER="lf_v2.12"
-MKIMAGE_SRC_GIT_ID='8737dc3604e430c902c455da344975e22a594ffe' #refer to 'imx-mkimage_git.inc' in Yocto
-ATF_SRC_GIT_ID='6adc4c6f6d9e8bb647aa5b30112d0ce264900052' #refer to 'imx-atf_2.12.bb' in Yocto
-DDR_FW_VER="8.29-8741a3b" #refer to the name of 'firmware-imx-8m_8.x.bb'
-ELE_FW_VER="2.0.3-286c884" ##refer to the "{PV of firmware-ele-imx_2.0.2.bb}"-"{IMX_SRCREV_ABBREV}"
+MKIMAGE_SRC_GIT_ID='be80fadd5e7988214149a2bc48daac1b0950d4c2' #refer to 'imx-mkimage_git.inc' in Yocto
+ATF_SRC_GIT_ID='a266ff458c2526a6474036a5c6648be6fdc54fe3' #refer to 'imx-atf_2.12.bb' in Yocto
+DDR_FW_VER="8.30-3fa84fd" #refer to the name of 'firmware-imx-8.x.bb'
+ELE_FW_VER="2.0.4-93492e0" ##refer to the "{PV of firmware-ele-imx_2.0.2.bb}"-"{IMX_SRCREV_ABBREV}"
 
 FSL_MIRROR="https://www.nxp.com/lgfiles/NMG/MAD/YOCTO"
 FIRMWARE_DIR="imx-boot_generation"
@@ -30,11 +30,11 @@ TWD=`pwd`
 ATF_BOOT_UART_BASE="0x30890000"
 
 # Config for i.mx95
-IMX_SM_GIT_REPO="https://github.com/TechNexion/imx-sm.git"
-IMX_SM_BRANCH_VER="tn-imx_6.12.34_2.1.0"
+IMX_SM_GIT_REPO="https://github.com/nxp-imx/imx-sm.git"
+IMX_SM_BRANCH_VER="master"
 IMX_SM_CONFIG="mx95evk"
 IMX_OEI_GIT_REPO="https://github.com/TechNexion/imx-oei.git"
-IMX_OEI_BRANCH_VER="tn-imx_6.12.34_2.1.0"
+IMX_OEI_BRANCH_VER="tn-imx_6.12.49_2.2.0"
 IMX_OEI_CONFIG="edm-imx95"
 ARM_TOOLCHAIN_VER_DEFAULT="14.2.rel1"
 
