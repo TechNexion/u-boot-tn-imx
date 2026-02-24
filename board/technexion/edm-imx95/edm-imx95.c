c7524b472aee8e501ca020a16e47d942b87a7842
Jack Lo <jack.lo@technexion.com>
2026-02-24 13:00:25 +0800

    arm: dts: imx95-edm-evm: enable LVDS panel and splash screen support
    Support LVDS display output on imx95-edm-evm by:
    1. Switching backlight control from PWM to GPIO mode.
    2. Adding LVDS panel node with 1280x800 display timings.
    3. Enabling DPU, LDB, and LVDS PHY related nodes.
    4. Implementing splash_screen_prepare() to load splash image from MMC.
    5. Updating defconfig to include LVDS, GPIO backlight, and splash source drivers.
    
    This allows the board to display a boot logo during the U-Boot stage.

--- board/technexion/edm-imx95/edm-imx95.c
+++ board/technexion/edm-imx95/edm-imx95.c
@@ -27,6 +27,57 @@
 #include "edm-imx95-ddr.h"
 #include "../common/periph_detect.h"
 
+#ifdef CONFIG_SPLASH_SCREEN
+#include <splash.h>
+
+static struct splash_location imx_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x100000,
+	},
+	{
+		.name = "mmc_fs",
+		.storage = SPLASH_STORAGE_MMC,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "usb_fs",
+		.storage = SPLASH_STORAGE_USB,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+	{
+		.name = "sata_fs",
+		.storage = SPLASH_STORAGE_SATA,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "0:1",
+	},
+};
+
+/*This function is defined in common/splash.c.
+  Declare here to remove warning. */
+int splash_video_logo_load(void);
+
+int splash_screen_prepare(void)
+{
+	int ret;
+	pr_err("splash_screen_prepare\n");
+	imx_splash_locations[1].devpart[0] = mmc_get_env_dev() + '0';
+	pr_err("splash_screen_prepare, devpart:%s\n", imx_splash_locations[1].devpart);
+	ret = splash_source_load(imx_splash_locations, ARRAY_SIZE(imx_splash_locations));
+	if (!ret)
+		return 0;
+	else {
+		printf("\nNo splash.bmp in boot partition!!\n");
+		printf("Using default logo!!\n\n");
+		return splash_video_logo_load();
+	}
+}
+#endif /* CONFIG_SPLASH_SCREEN */
+
 enum typec_cc_polarity {
 	TYPEC_POLARITY_CC1,
 	TYPEC_POLARITY_CC2,
