932cff917128c8bdf422877d20b47e3e31537f75
Jack Lo <jack.lo@technexion.com>
2026-06-25 11:50:57 +0800

    refactor: refactor i.MX8 U-Boot installation script and fix ATF hash
    Refactor install_uboot_imx8.sh to modernise code structure and improve
    reliability. Key changes include:
    - Switch interpreter from /bin/sh to /bin/bash to use bash-specific
      features like ${FUNCNAME} and ${BASH_SOURCE}.
    - Replace multi-branch if-else statements with clean case structures.
    - Extract redundant command executions, file copies, and git clones
      into reusable wrapper functions with robust error checking.
    - Fix invalid ATF_SRC_GIT_ID hash that caused checkout failures.
    - Introduce conditional build/flash logic and new CLI options (-i, -I).

--- install_uboot_imx8.sh
+++ install_uboot_imx8.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #################################################################################
 # Copyright 2018 Technexion Ltd.
 #
@@ -8,6 +8,8 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 #################################################################################
+script_path="$(readlink -f "${BASH_SOURCE[0]}")"
+script_dir="$(cd "$(dirname "$script_path")" && pwd)"
 
 DRIVE=/dev/sdX
 
@@ -21,7 +23,8 @@ DRIVE=/dev/sdX
 BRANCH_VER="lf-5.10.72_2.2.0" #branch used by imx-mkimage and imx-atf under meta-imx
 ATF_BRANCH_VER="lf_v2.4"
 MKIMAGE_SRC_GIT_ID='1112c88470f339dc631e2a7117087f416af6d6b5' #refer to 'imx-mkimage_git.inc' in Yocto
-ATF_SRC_GIT_ID='7a277c8a1a21ff921d217889dde6a9f84e6d2168' #refer to 'imx-atf_2.0.bbappend' in Yocto
+#ATF_SRC_GIT_ID='7a277c8a1a21ff921d217889dde6a9f84e6d2168' #refer to 'imx-atf_2.0.bbappend' in Yocto 此ID 不存在
+ATF_SRC_GIT_ID='5f3ed37b85745d360f6a942fa97df04e05779ffe' #
 DDR_FW_VER="8.14" #refer to the name of 'firmware-imx-8_8.x.bb'
 
 FSL_MIRROR="https://www.nxp.com/lgfiles/NMG/MAD/YOCTO"
@@ -38,58 +41,128 @@ TWD=`pwd`
 setup_platform()
 {
 	SOC=$( echo "${DTBS}" | cut -d' ' -f1 | grep -o 'imx8m[mqpn]\?' )
-	if [ ${SOC} = "imx8m" ] || [ ${SOC} = "imx8mq" ] ; then
-		PLATFORM="imx8mq"
-		SOC_TARGET="iMX8M"
-		SOC_DIR="iMX8M"
-		IMX_BOOT_SEEK="33"
-	elif [ ${SOC} = "imx8mm" ] ; then
-		PLATFORM="imx8mm"
-		SOC_TARGET="iMX8MM"
-		SOC_DIR="iMX8M"
-		IMX_BOOT_SEEK="33"
-	elif [ ${SOC} = "imx8mp" ] ; then
-		PLATFORM="imx8mp"
-		SOC_TARGET="iMX8MP"
-		SOC_DIR="iMX8M"
-		IMX_BOOT_SEEK="32"
-	elif [ ${SOC} = "imx8mn" ] ; then
-		PLATFORM="imx8mn"
-		SOC_TARGET="iMX8MN"
-		SOC_DIR="iMX8M"
-		IMX_BOOT_SEEK="32"
-	else
-		printf "Targest SOC isn't supported by this script\n"
-		exit 1
+	case "${SOC}" in
+		imx8m|imx8mq)
+			PLATFORM="imx8mq"
+			SOC_TARGET="iMX8M"
+			SOC_DIR="iMX8M"
+			IMX_BOOT_SEEK="33"
+			;;
+		imx8mm)
+			PLATFORM="imx8mm"
+			SOC_TARGET="iMX8MM"
+			SOC_DIR="iMX8M"
+			IMX_BOOT_SEEK="33"
+			;;
+		imx8mp)
+			PLATFORM="imx8mp"
+			SOC_TARGET="iMX8MP"
+			SOC_DIR="iMX8M"
+			IMX_BOOT_SEEK="32"
+			;;
+		imx8mn)
+			PLATFORM="imx8mn"
+			SOC_TARGET="iMX8MN"
+			SOC_DIR="iMX8M"
+			IMX_BOOT_SEEK="32"
+			;;
+		*)
+			printf "Target SOC isn't supported by this script\n"
+			exit 1
+			;;
+	esac
+}
+
+log_msg() {
+  echo "${FUNCNAME[1]} $1"
+}
+
+info_msg() {
+  echo "[INFO][${FUNCNAME[1]}] $1"
+}
+error_msg() {
+  echo "[ERROR][${FUNCNAME[1]}] $1"
+  exit -1
+}
+warning_msg() {
+  echo "[WARN][${FUNCNAME[1]}] $1"
+}
+debug_msg() {
+	if [ "$DEBUG_MODE" == "yes" ]; then
+	echo "[DBG][${FUNCNAME[1]}] $1"
 	fi
+}
+
+run_cmd() {
+	local label=$1
+	local err_exit=$2
+	local cmd_str=$3
+
+	info_msg "l:${label}, Running command: $cmd_str"
+	eval "$cmd_str"
+	local ret=$?
+	if [ "$ret" == "0" ]; then
+		return 0
+	fi
+
+	if [ "$ret" != "0" ]; then
+		error_msg "[$label] Command failed: $cmd_str"
+	fi
+
+	if [ "$err_exit" == "yes" ]; then
+		exit -1
+	fi
+	return 1
+}
+
+copy_file() {
+	local src=$1
+	local dest=$2
+	printf "Copy file $src to $dest ..."
+	cp $src $dest
+	if [ "$?" != "0" ]; then
+		error_msg "Fails to copy ${src} to ${dest}"
+		exit -1
+	fi
+	echo " success"
+}
+
+git_clone() {
+	local id=$1
+	local repo=$2
+	local branch=$3
+	local hash=$4
+	local dir=$5
+
+	if [ -d $dir ]; then
+		info_msg "Git $id have exist, skip clone"
+		return
+	fi
+
+	info_msg "Start clone $id $dir..."
+	mkdir -p $dir
+	pushd $dir
+	run_cmd "git clone $id" "yes" "git clone $repo -b $branch ."
+	run_cmd "git checkout $id" "yes" "git checkout -b ${branch}_local ${hash}"
+	popd
+	info_msg "Finish clone $id finish"
 }
 
 install_firmware()
 {
 	cd ${TWD}
 	#Get and Build NXP imx-mkimage tool
-	if [ ! -d ${MKIMAGE_DIR} ] ; then
-		git clone https://github.com/nxp-imx/imx-mkimage.git -b ${BRANCH_VER} || printf "Fails to fetch imx-mkimage source code \n"
-		cd imx-mkimage
-		git checkout -b ${BRANCH_VER}_local ${MKIMAGE_SRC_GIT_ID}
-	fi
-	cd ${TWD}
-	#Collect required firmware files to generate bootable binary
-	if [ ! -d ${FIRMWARE_DIR} ] ; then
-		mkdir ${FIRMWARE_DIR}
-	fi
 
+	git_clone "imx-mkimage" "https://github.com/nxp-imx/imx-mkimage.git" "${BRANCH_VER}" "${MKIMAGE_SRC_GIT_ID}" "${script_dir}/${MKIMAGE_DIR}"
+
+	cd ${TWD}
 	cd ${FIRMWARE_DIR} && FWD=`pwd`
-	
 	#Get, build and copy the ARM Trusted Firmware
-	if [ ! -d imx-atf ] ; then
-		git clone https://github.com/nxp-imx/imx-atf.git -b ${ATF_BRANCH_VER} || printf "Fails to fetch ATF source code \n"
-		cd imx-atf
-		git checkout -b ${BRANCH_VER}_local ${ATF_SRC_GIT_ID}
-	fi
+	git_clone "imx-atf" "https://github.com/nxp-imx/imx-atf.git" "${ATF_BRANCH_VER}" "${ATF_SRC_GIT_ID}" "${FIRMWARE_DIR}/imx-atf"
 
 	PWD=$(pwd)
-	[ -n "${PWD##*imx-atf}" ] && cd imx-atf
+	cd ${FIRMWARE_DIR}/imx-atf
+	# [ -n "${PWD##*imx-atf}" ] && cd imx-atf
 	if ( git diff-index --quiet HEAD -- plat/imx/imx8mm/imx8mm_bl31_setup.c ); then
 		if [ -z "${DTBS##*imx8mm-axon*}" ]; then
 			# AXON: Change UART2 base address to UART1 and released UART4 from M4
@@ -105,41 +178,39 @@ install_firmware()
 		fi
 	fi
 
-	if [ ! -f build/${PLATFORM}/release/bl31.bin ] ; then
-		rm -rf build
-		make PLAT=${PLATFORM} bl31 || printf "Fails to build ATF firmware \n"
-	fi	
-	if [ -f build/${PLATFORM}/release/bl31.bin ] ; then
-		printf "Copy build/${PLATFORM}/release/bl31.bin to $MKIMAGE_DIR \n"
-		cp build/${PLATFORM}/release/bl31.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-	else
-		printf "Cannot find release/bl31.bin \n"
-	fi
-	
+	run_cmd "Delete bl31.bin" "no" "find -name \"bl31.bin\" -delete"
+	run_cmd "Build imx-atf" "yes" "make -j$(nproc) PLAT=${PLATFORM} bl31"
+	mkdir -p ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/
+	copy_file build/${PLATFORM}/release/bl31.bin "${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/"
+
 	#Get and copy the DDR and HDMI firmware
 	cd ${FWD}
 	if [ ! -d firmware-imx-${DDR_FW_VER} ] ; then
+		info_msg "Download DDR firmware..."
 		wget ${FSL_MIRROR}/firmware-imx-${DDR_FW_VER}.bin && \
 		chmod +x firmware-imx-${DDR_FW_VER}.bin && \
 		./firmware-imx-${DDR_FW_VER}.bin || \
 		printf "Fails to fetch DDR firmware \n"
 	fi
 
+	local src_base_dir="firmware-imx-${DDR_FW_VER}/firmware"
+	local out_dir="${TWD}/${MKIMAGE_DIR}/${SOC_DIR}"
 	if [ -d firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys ] ; then
 		if [ ${SOC} = "imx8mp" ] ; then
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_1d_dmem_202006.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_1d_imem_202006.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_2d_dmem_202006.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_2d_imem_202006.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_1d_dmem_202006.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_1d_imem_202006.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_2d_dmem_202006.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_2d_imem_202006.bin "${out_dir}/"
 		else
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_1d_dmem.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_1d_imem.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_2d_dmem.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-			cp firmware-imx-${DDR_FW_VER}/firmware/ddr/synopsys/lpddr4_pmu_train_2d_imem.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_1d_dmem.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_1d_imem.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_2d_dmem.bin "${out_dir}/"
+				copy_file ${src_base_dir}/ddr/synopsys/lpddr4_pmu_train_2d_imem.bin "${out_dir}/"
 		fi
 			cp firmware-imx-${DDR_FW_VER}/firmware/hdmi/cadence/signed_hdmi_imx8m.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
 	else
 		printf "Cannot find DDR firmware \n"
+		exit -1
 	fi
 }
 
@@ -147,46 +218,51 @@ install_uboot_dtb()
 {
 	#Copy uboot binary
 	cd ${TWD}
-	if [ -f u-boot-nodtb.bin ] ; then
-		cp u-boot-nodtb.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-	else
-		printf "Cannot find u-boot-nodtb.bin. Please build u-boot first! \n"
-	fi
+	copy_file u-boot-nodtb.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/
 
 	#Copy SPL binary
 	cd ${TWD}
-	if [ -f spl/u-boot-spl.bin ] ; then
-		cp spl/u-boot-spl.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-	else
-		printf "Cannot find spl/u-boot-spl.bin. Please build u-boot first! \n"
-	fi
+	copy_file spl/u-boot-spl.bin ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/
 
 	#Copy device tree file
 	cd ${TWD}
 	for DTB in ${DTBS}
 	do
-		if [ -f arch/arm/dts/${DTB} ] ; then
-			cp arch/arm/dts/${DTB} ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}
-		else
-			printf "Cannot find arch/arm/dts/${DTB} . Please build u-boot first! \n"
-		fi
+		copy_file arch/arm/dts/${DTB} ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/
 	done
+}
+
+
+u_boot_build() {
+	info_msg "Start build u-boot"
+	cd ${script_dir}
+
+	SOC=$( echo "${DTBS}" | cut -d' ' -f1 | grep -o 'imx8m[mqpn]\?' )
+	case "${SOC}" in
+		imx8mm|imx8mq)
+		run_cmd "Build imx-boot ${SOC} config" "yes" "make edm-g-imx8mm_defconfig"
+		run_cmd "Build imx-boot ${SOC}"        "yes" "make -j$(nproc)"
+		;;
+	    *)
+		error_msg "Not support build u-boot type:${SOC}"
+		;;
+	esac
+
+	info_msg "finish build u-boot"
 }
 
 generate_imx_boot()
 {
 	cd ${TWD}
 	#Before generating the flash.bin, transfer the mkimage generated by U-Boot to iMX8M folder
-	if [ -f tools/mkimage ] ; then
-		cp tools/mkimage ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/mkimage_uboot
-	else
-		printf "Cannot find tools/mkimage. Please build u-boot first! \n"
-	fi
+	copy_file tools/mkimage ${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/mkimage_uboot
 
 	#Generate bootable binary (This binary contains SPL and u-boot.bin) for flashing
 	cd ${MKIMAGE_DIR}
-	make SOC=${SOC_TARGET} dtbs="${DTBS}" ${MKIMAGE_TARGET} && \
-	printf "Make target: ${MKIMAGE_TARGET} and generate flash.bin... \n" || printf "Fails to generate flash.bin... \n"
+	run_cmd "Generate ${MKIMAGE_TARGET} image" "yes" "make SOC=${SOC_TARGET} dtbs=\"${DTBS}\" clean"
+	run_cmd "Generate ${MKIMAGE_TARGET} image" "yes" "make SOC=${SOC_TARGET} dtbs=\"${DTBS}\" ${MKIMAGE_TARGET} "
+	# printf "Make target: ${MKIMAGE_TARGET} and generate flash.bin... \n" || printf "Fails to generate flash.bin... \n"
+	info_msg "Generate ${MKIMAGE_TARGET} image finish..."
 }
 
 flash_imx_boot()
@@ -198,8 +274,13 @@ flash_imx_boot()
 	fi
 	sudo umount ${DRIVE}?
 	sleep 0.1
-	sudo dd if=${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/${IMX_BOOT} of=${DRIVE} bs=1k seek=${IMX_BOOT_SEEK} oflag=dsync status=progress && \
-	printf "Flash flash.bin... \n" || printf "Fails to flash flash.bin... \n"
+	run_cmd "Flash ${MKIMAGE_TARGET} image" "yes" "sudo dd if=${TWD}/${MKIMAGE_DIR}/${SOC_DIR}/${IMX_BOOT} of=${DRIVE} bs=1k seek=${IMX_BOOT_SEEK} oflag=dsync status=progress"
+	sleep 2
+	echo "sudo umount /media/$(id -un)/boot"
+	sudo umount /media/$(id -un)/boot
+	sudo eject ${DRIVE}
+	# if [ "$?" != "0" ]; then
+	# 	printf "Flash flash.bin... \n" || printf "Fails to flash flash.bin... \n"
 }
 
 usage()
@@ -220,7 +301,7 @@ usage()
     i.mx8MM:
     * PICO-IMX8MM with PICO-PI-IMX8 baseDTBS:
     ./install_uboot_imx8.sh -b imx8mm-pico-pi.dtb -b imx8mm-pico-wizard.dtb -d /dev/sdX
-	
+
     * EDM-G-IMX8MM with WB:
     ./install_uboot_imx8.sh -b imx8mm-edm-g-wb.dtb -d /dev/sdX
 
@@ -260,11 +341,11 @@ print_settings()
 	"
 	echo "The disk path to flash u-boot: $DRIVE"
 	echo "The default DTB name: ${DTBS}"
-	echo "Make target: ${PLATFORM}"
-	echo "Make target: ${MKIMAGE_TARGET}"
+	echo "Make -j$(nproc) target: ${PLATFORM}"
+	echo "Make -j$(nproc) target: ${MKIMAGE_TARGET}"
 	echo "SOC platform: ${SOC}"
 	echo "*************************************************************
-		
+
 	"
 }
 
@@ -273,27 +354,36 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-while getopts "tcfhd:b:" OPTION
+while getopts "tcfhd:b:iI" OPTION
 do
     case $OPTION in
-        d) 
+        d)
            DRIVE="$OPTARG"
            ;;
-        b) 
+        b)
            DTBS="$DTBS $OPTARG"
            ;;
-        t) 
+        t)
            MKIMAGE_TARGET='flash_spl_uboot';
            ;;
-        f) 
+        f)
            MKIMAGE_TARGET='flash_evk_flexspi';
            ;;
-		c) 
+		c)
 		   rm -rf ${FIRMWARE_DIR} ${MKIMAGE_DIR}
 		   echo "Clean ${FIRMWARE_DIR} ${MKIMAGE_DIR}..."
 		   exit
 		   ;;
-        ?|h) usage
+		i) 	echo "Flash to $DRIVE"
+		    setup_platform
+		    flash_imx_boot
+		    exit
+			;;
+		I)
+		   export NOT_BUILD=1
+		   ;;
+		?|h)
+			usage
            exit
            ;;
     esac
@@ -301,7 +391,7 @@ done
 
 DTBS=$(echo ${DTBS} | cut -c 1-)
 
-if [ "$(id -u)" = "0" ]; then
+if [ "$(id -u)" == "0" ]; then
    echo "This script can not be run as root"
    exit 1
 fi
@@ -315,8 +405,14 @@ fi
 
 setup_platform
 print_settings
-install_firmware
-install_uboot_dtb
-generate_imx_boot
-flash_imx_boot
 
+if [ "$NOT_BUILD" != "1" ]; then
+	u_boot_build
+	install_firmware
+	install_uboot_dtb
+	generate_imx_boot
+fi
+
+if [ "$DRIVE" != "/dev/sdX" ]; then
+	flash_imx_boot
+fi
