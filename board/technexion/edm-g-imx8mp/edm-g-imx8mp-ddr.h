be11b428f72261aecb8a8e5aec480167189d8581
Ray Chang <ray.chang@technexion.com>
2026-06-26 14:42:13 +0800

    edm-g-imx8mp: add support for 4GB LPDDR4 1-rank memory variant
    Introduce support for a new 4GB LPDDR4 Single-Rank (1-rank) memory configuration.
    The hardware is distinguished using BOARD_ID[2:0] = 010b (0x2).

--- board/technexion/edm-g-imx8mp/edm-g-imx8mp-ddr.h
+++ board/technexion/edm-g-imx8mp/edm-g-imx8mp-ddr.h
@@ -12,6 +12,7 @@
 BOARD_ID2    BOARD_ID1   BOARD_ID0
    1            0            1       8G LPDDR4
    0            0            1       6G LPDDR4
+   0            1            0       4G LPDDR4(1-rank)
    1            1            0       4G LPDDR4
    0            0            0       2G LPDDR4
    1            0            0       1G LPDDR4
@@ -20,6 +21,7 @@ enum {
 	LPDDR4_8GB = 0x5,
 	LPDDR4_6GB = 0x1,
 	LPDDR4_4GB = 0x6,
+	LPDDR4_4GB_1RANK = 0x2,
 	LPDDR4_2GB = 0x0,
 	LPDDR4_1GB = 0x4,
         LPDDR4_UNKNOWN = 0xf,
